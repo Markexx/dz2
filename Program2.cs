465298dd421220f00b9e95d2a3c51d1875807456
Markexx <94964951+Markexx@users.noreply.github.com>
2024-05-13 12:39:43 +0200

    Add files via upload

---
+++ Program2.cs
@@ -0,0 +1,17 @@
+﻿using dz2___2._zadatak;
+class Program
+{
+    static void Main(string[] args)
+    {
+        LoginPageFactory factory = new ChromeLoginPageFactory();
+        LoginPage page = factory.CreatePage();
+
+        WebElement loginButton = page.loginButton();
+        WebElement usernameInput = page.usernameInput();
+        WebElement passwordInput = page.passwordInput();
+
+        loginButton.Click();
+        usernameInput.Click();
+        passwordInput.Click();
+    }
+}
